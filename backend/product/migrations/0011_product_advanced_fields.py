92b066de3e7786467bfd7facb33f4396758517cf
xsnvna <xusanovna2006@gmail.com>
2026-06-12 17:02:07 +0500

    Update migration with missing models and remaining product fields

--- backend/product/migrations/0011_product_advanced_fields.py
+++ backend/product/migrations/0011_product_advanced_fields.py
@@ -1,5 +1,6 @@
 # Generated manually
 from django.db import migrations, models
+import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
@@ -52,5 +53,47 @@ class Migration(migrations.Migration):
             model_name='product',
             name='size',
             field=models.CharField(blank=True, max_length=50, null=True),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='rating',
+            field=models.DecimalField(blank=True, decimal_places=2, default=0, max_digits=7, null=True),
+        ),
+        migrations.AddField(
+            model_name='product',
+            name='numReviews',
+            field=models.IntegerField(blank=True, default=0, null=True),
+        ),
+        migrations.CreateModel(
+            name='ProductImage',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('image', models.ImageField(blank=True, null=True, upload_to='')),
+                ('product', models.ForeignKey(on_delete=models.deletion.CASCADE, related_name='images', to='product.product')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(blank=True, max_length=200, null=True)),
+                ('rating', models.IntegerField(blank=True, default=0, null=True)),
+                ('comment', models.TextField(blank=True, null=True)),
+                ('createdAt', models.DateTimeField(auto_now_add=True)),
+                ('product', models.ForeignKey(null=True, on_delete=models.deletion.SET_NULL, related_name='reviews', to='product.product')),
+                ('user', models.ForeignKey(null=True, on_delete=models.deletion.SET_NULL, to='auth.user')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Wishlist',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('createdAt', models.DateTimeField(auto_now_add=True)),
+                ('product', models.ForeignKey(on_delete=models.deletion.CASCADE, to='product.product')),
+                ('user', models.ForeignKey(on_delete=models.deletion.CASCADE, to='auth.user')),
+            ],
+            options={
+                'unique_together': {('user', 'product')},
+            },
         ),
     ]
